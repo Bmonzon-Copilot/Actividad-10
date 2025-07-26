812e619f0379b050ba6a5038c426a34134451254
Bmonzon <bmonzon1024@gmail.com>
2025-07-25 21:01:43 -0600

    commit 1

---
+++ Actividad_10.py
@@ -0,0 +1,11 @@
+def menu():
+    while True:
+        print("\n******MENU******")
+        print("1. Invertir una cadena de texto")
+        print("2. Calcular la suma de los primeros N números naturales")
+        print("3. Imprimir una cuenta regresiva desde N hasta 1")
+        print("4. Sumar los dígitos de un número")
+        print("5. Contar cuántos dígitos tiene un número")
+        print("6. Salir")
+        opcion = input("Ingrese una opcion: ").strip()
+menu()
